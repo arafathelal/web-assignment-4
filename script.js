f4ff4d6c2e9538d5ca5559c402bca3fe81d5fcb0
Arafat <arafathelal7@gmail.com>
2026-02-24 15:43:58 +0600

    interview and reject button function added

--- script.js
+++ script.js
@@ -74,3 +74,68 @@ rejBtn.onclick = function () {
 
 updateCounts();
 showTab("all");
+
+//interview, reject button
+
+for (let i = 0; i < jobCards.length; i++) {
+
+    const card = jobCards[i];
+
+    const interviewBtn = card.querySelector(".interviewBtn");
+    const rejectBtn = card.querySelector(".rejectBtn");
+    const deleteBtn = card.querySelector(".deleteBtn");
+    const badge = card.querySelector(".statusBadge");
+
+
+    //interview
+    interviewBtn.onclick = function () {
+
+        card.classList.remove("rejected");
+
+        if (card.classList.contains("interview")) {
+
+            card.classList.remove("interview");
+            interviewBtn.classList.remove("bg-green-600", "text-white");
+
+            badge.innerText = "NOT APPLIED";
+            badge.className ="statusBadge bg-blue-100 text-blue-700 px-3 py-1 rounded-md text-sm inline-block mt-2";
+
+        } else {
+
+            card.classList.add("interview");
+            interviewBtn.classList.add("bg-green-600", "text-white");
+            rejectBtn.classList.remove("bg-red-600", "text-white");
+
+            badge.innerText = "INTERVIEW";
+            badge.className ="statusBadge bg-green-100 text-green-700 px-3 py-1 rounded-md text-sm inline-block mt-2";
+        }
+
+        showTab(currentTab);
+    }
+
+    //reject
+    rejectBtn.onclick = function () {
+
+        card.classList.remove("interview");
+
+        if (card.classList.contains("rejected")) {
+
+            card.classList.remove("rejected");
+            rejectBtn.classList.remove("bg-red-600", "text-white");
+
+            badge.innerText = "NOT APPLIED";
+            badge.className ="statusBadge bg-blue-100 text-blue-700 px-3 py-1 rounded-md text-sm inline-block mt-2";
+
+        } else {
+
+            card.classList.add("rejected");
+            rejectBtn.classList.add("bg-red-600", "text-white");
+            interviewBtn.classList.remove("bg-green-600", "text-white");
+
+            badge.innerText = "REJECTED";
+            badge.className = "statusBadge bg-red-100 text-red-700 px-3 py-1 rounded-md text-sm inline-block mt-2";
+        }
+
+        showTab(currentTab);
+    }
+}
